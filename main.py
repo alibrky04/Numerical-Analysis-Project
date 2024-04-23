ec62319b6a9dd85bd930255a92edc7403cd6ed32
Ali Berkay Görgülü <aliberkay.gorgulu@hotmail.com>
2024-04-23 19:00:06 +0300

    Question 1 updated.

--- main.py
+++ main.py
@@ -2,24 +2,10 @@ from questions.question1 import question1
 import matplotlib.pyplot as plt
 import numpy as np
 
-Q1FINAL = 100
+Q1LIMIT = 100
 
-q1 = question1()
+q1 = question1(Q1LIMIT)
 
-q1.calculateUntil(Q1FINAL)
+q1.calculateUntil()
 
-for i in range(Q1FINAL):
-    print(f"Element {i + 1} : " + str(q1.calculatedElements[i]))
-
-plt.plot(np.arange(1, 101), q1.calculatedElements)
-plt.yscale('log')
-plt.xlabel('Element')
-plt.ylabel('Value')
-plt.title('Elements while x_n = (1 - sqrt(3))^(n-1)')
-
-custom_ticks = [1] + list(range(5, 101, 5))
-custom_tick_labels = [f'n{i}' for i in custom_ticks]
-plt.xticks(custom_ticks, custom_tick_labels)
-
-plt.grid(True)
-plt.show()
+q1.showResult()
--- questions/question1.py
+++ questions/question1.py
@@ -1,10 +1,30 @@
 import math as m
+import matplotlib.pyplot as plt
+import numpy as np
 
 class question1:
-    def __init__(self):
+    def __init__(self, limit = 3):
         self.calculatedElements = [1, 1 - m.sqrt(3)]
+        self.limit = limit
     
-    def calculateUntil(self, lastNumber = 3):
-        for i in range(2, lastNumber):
+    def calculateUntil(self):
+        for i in range(2, self.limit):
             newElement = 2 * (self.calculatedElements[i - 1] + self.calculatedElements[i - 2])
             self.calculatedElements.append(newElement)
+
+    def showResult(self):
+        for i in range(self.limit):
+            print(f"Element {i + 1} : " + str(self.calculatedElements[i]))
+
+        plt.plot(np.arange(1, 101), self.calculatedElements)
+        plt.yscale('log')
+        plt.xlabel('Element')
+        plt.ylabel('Value')
+        plt.title('Elements while x_n = (1 - sqrt(3))^(n-1)')
+
+        custom_ticks = [1] + list(range(5, 101, 5))
+        custom_tick_labels = [f'n{i}' for i in custom_ticks]
+        plt.xticks(custom_ticks, custom_tick_labels)
+
+        plt.grid(True)
+        plt.show()
